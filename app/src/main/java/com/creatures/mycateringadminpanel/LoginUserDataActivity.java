5931311792c286cb314faa9221a7923f1c61aac6
Preet Ojha <ozapreet08@gmail.com>
2022-04-27 22:44:05 +0530

    Admin Panel UI Updated

---
+++ app/src/main/java/com/creatures/mycateringadminpanel/LoginUserDataActivity.java
@@ -0,0 +1,14 @@
+package com.creatures.mycateringadminpanel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginUserDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_user_data);
+    }
+}
